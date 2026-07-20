403bbdeee3668a9f0f602f595dcd30fe0e76a772
darjss <darjsavid@gmail.com>
2026-07-21 00:38:55 +0800

    chore: remove unused ui exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,2 @@
-export { Button, type ButtonProps, buttonVariants } from "./components/button";
+export { Button } from "./components/button";
 export { AltArrowDown, AltArrowUp } from "@solar-icons/solid/category/arrows/Linear";
-export { Bag } from "@solar-icons/solid/category/shopping/Linear";
